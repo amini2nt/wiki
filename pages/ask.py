9a06f8f35d31163ed9a02459cfe479d048c14292
Vladimir Blagojevic <dovlex@gmail.com>
2022-01-26 11:45:45 +0100

    Add answer to context sentence similarity

--- pages/ask.py
+++ pages/ask.py
@@ -1,3 +1,4 @@
+import colorsys
 import json
 import time
 import nltk
@@ -39,9 +40,15 @@ def request_context_passages(question, header):
     return json.loads(response.content.decode("utf-8"))
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True, show_spinner=False)
 def get_sentence_transformer():
     return SentenceTransformer('all-MiniLM-L6-v2')
+
+
+@st.cache(allow_output_mutation=True, show_spinner=False)
+def get_sentence_transformer_encoding(sentences):
+    model = get_sentence_transformer()
+    return model.encode([sentence for sentence in sentences], convert_to_tensor=True)
 
 
 def signJWT() -> Dict[str, str]:
@@ -57,6 +64,27 @@ def extract_sentences_from_passages(passages):
     for idx, node in enumerate(passages):
         sentences.extend(tokenize.sent_tokenize(node["text"]))
     return sentences
+
+
+def similarity_color_picker(similarity: float):
+    value = int(similarity * 100)
+    rgb = colorsys.hsv_to_rgb(value / 300., 1.0, 1.0)
+    return [round(255 * x) for x in rgb]
+
+
+def answer_to_context_similarity(generated_answer, context_passages, topk=3):
+    context_sentences = extract_sentences_from_passages(context_passages)
+    context_sentences_e = get_sentence_transformer_encoding(context_sentences)
+    answer_sentences = tokenize.sent_tokenize(generated_answer)
+    answer_sentences_e = get_sentence_transformer_encoding(answer_sentences)
+    search_result = util.semantic_search(answer_sentences_e, context_sentences_e, top_k=topk)
+    result = []
+    for idx, r in enumerate(search_result):
+        context = []
+        for idx_c in range(topk):
+            context.append({"source": context_sentences[r[idx_c]["corpus_id"]], "score": r[idx_c]["score"]})
+        result.append({"answer": answer_sentences[idx], "context": context})
+    return result
 
 
 def app():
@@ -183,6 +211,7 @@ def app():
 
         elif data and len(data) > 0:
             generated_answer = data[0]['generated_text']
+            sentence_similarity = answer_to_context_similarity(generated_answer, context_passages, topk=3)
 
             st.markdown(
                 " ".join([
@@ -230,20 +259,21 @@ def app():
             with col1:
                 st.subheader("Context")
             with col2:
-                selection = st.selectbox(label='', options=('Paragraphs', 'Sentences'))
+                selection = st.selectbox(label='Scope', options=('Paragraphs', 'Sentences', 'Answer Similarity'))
 
             question_e = model.encode(question, convert_to_tensor=True)
             if selection == "Paragraphs":
-                context_e = model.encode([d["text"] for d in context_passages], convert_to_tensor=True)
+                sentences = extract_sentences_from_passages(context_passages)
+                context_e = get_sentence_transformer_encoding(sentences)
                 scores = util.cos_sim(question_e.repeat(context_e.shape[0], 1), context_e)
                 similarity_scores = scores[0].squeeze().tolist()
                 for idx, node in enumerate(context_passages):
                     node["answer_similarity"] = "{0:.2f}".format(similarity_scores[idx])
                 context_passages = sorted(context_passages, key=lambda x: x["answer_similarity"], reverse=True)
                 st.json(context_passages)
-            else:
+            elif selection == "Sentences":
                 sentences = extract_sentences_from_passages(context_passages)
-                sentences_e = model.encode([sentence for sentence in sentences], convert_to_tensor=True)
+                sentences_e = get_sentence_transformer_encoding(sentences)
                 scores = util.cos_sim(question_e.repeat(sentences_e.shape[0], 1), sentences_e)
                 sentence_similarity_scores = scores[0].squeeze().tolist()
                 result = []
@@ -252,6 +282,8 @@ def app():
                         {"text": sentence, "answer_similarity": "{0:.2f}".format(sentence_similarity_scores[idx])})
                 context_sentences = json.dumps(sorted(result, key=lambda x: x["answer_similarity"], reverse=True))
                 st.json(context_sentences)
+            else:
+                st.json(sentence_similarity)
 
         else:
             unknown_error = f"{data}"
